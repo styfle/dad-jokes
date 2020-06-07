c5dd5d725f6b2c77bd361ae02c780e2c7a6653d6
Steven <steven@ceriously.com>
2020-06-07 17:40:28 -0400

    Add website

---
+++ build.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const readme = fs.readFileSync('./readme.md', 'utf8');
+
+const data = readme
+    .split('---')
+    .map(block => (
+        {
+            lines: block.split('\n').filter(Boolean)
+        }
+    ));
+
+const json = JSON.stringify(data);
+fs.writeFileSync('./public/jokes.json', json);
