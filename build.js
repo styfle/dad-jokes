375f3c11243b6e2c4e6689e4c46091e97c24d71d
Steven <steven@ceriously.com>
2020-06-07 17:54:22 -0400

    format

--- build.js
+++ build.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 const readme = fs.readFileSync('./readme.md', 'utf8');
 
 const data = readme
-    .split('---')
-    .map(block => (
-        {
-            lines: block.split('\n').filter(Boolean)
-        }
-    ));
+	.split('---')
+	.map(block => (
+	  {
+		  lines: block.split('\n').filter(Boolean)
+	  }
+	));
 
 const json = JSON.stringify(data);
 fs.writeFileSync('./public/jokes.json', json);
+
--- public/script.js
+++ public/script.js
@@ -1,15 +1,12 @@
-async function fetchJokes() {
-    const res = await fetch('./jokes.json');
-    return res.json();
-}
-
 async function main() {
-    const jokes = await fetchJokes();
-    const i = Math.floor(Math.random() * 100);
-    const { lines } = jokes[i];
-    const el = document.querySelector('#joke');
-    el.innerHTML = lines.map(line => line.replace(/\*\*Q:\*\*/g, '').replace(/\*\*A:\*\*/g, '')).join('<p>');
-    document.querySelector('h1').innerText = 'Joke #' + i;
+	const res = await fetch('./jokes.json');
+	const jokes = await res.json();
+	const i = Math.floor(Math.random() * jokes.length);
+	const { lines } = jokes[i];
+	const el = document.querySelector('#joke');
+	el.innerHTML = lines.map(line => line.replace(/\*\*Q:\*\*/g, '').replace(/\*\*A:\*\*/g, '')).join('<p>');
+	document.querySelector('h1').innerText = 'Joke #' + i;
 }
 
-main()
+main();
+
