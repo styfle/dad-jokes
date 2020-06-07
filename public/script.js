b68f18065d8eac6b9daed3144dc21bc2c403a9ea
Steven <steven@ceriously.com>
2020-06-07 18:21:52 -0400

    Add favicon and styles

--- public/script.js
+++ public/script.js
@@ -1,12 +1,23 @@
-async function main() {
+let jokes = [];
+let index = 0;
+
+async function init() {
 	const res = await fetch('./jokes.json');
-	const jokes = await res.json();
-	const i = Math.floor(Math.random() * jokes.length);
+	jokes = await res.json();
+	setJoke();
+}
+
+function setJoke(i = Math.floor(Math.random() * jokes.length))
+	index = i;
 	const { lines } = jokes[i];
 	const el = document.querySelector('#joke');
 	el.innerHTML = lines.map(line => line.replace(/\*\*Q:\*\*/g, '').replace(/\*\*A:\*\*/g, '')).join('<p>');
 	document.querySelector('h1').innerText = 'Joke #' + i;
 }
 
-main();
 
+document.querySelector('#prev').onclick = () => setJoke(index - 1);
+document.querySelector('#random').onclick = () => setJoke();
+document.querySelector('#next').onclick = () => setJoke(index + 1);
+
+init();
