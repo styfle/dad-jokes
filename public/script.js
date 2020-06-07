81a21e4961b7eb35ab8985d1440aa8ed85334d0b
Steven <steven@ceriously.com>
2020-06-07 18:23:22 -0400

    Fix typo

--- public/script.js
+++ public/script.js
@@ -2,19 +2,18 @@ let jokes = [];
 let index = 0;
 
 async function init() {
-	const res = await fetch('./jokes.json');
-	jokes = await res.json();
-	setJoke();
+  const res = await fetch('./jokes.json');
+  jokes = await res.json();
+  setJoke();
 }
 
-function setJoke(i = Math.floor(Math.random() * jokes.length))
-	index = i;
-	const { lines } = jokes[i];
-	const el = document.querySelector('#joke');
-	el.innerHTML = lines.map(line => line.replace(/\*\*Q:\*\*/g, '').replace(/\*\*A:\*\*/g, '')).join('<p>');
-	document.querySelector('h1').innerText = 'Joke #' + i;
+function setJoke(i = Math.floor(Math.random() * jokes.length)) {
+  index = i;
+  const { lines } = jokes[i];
+  const el = document.querySelector('#joke');
+  el.innerHTML = lines.map(line => line.replace(/\*\*Q:\*\*/g, '').replace(/\*\*A:\*\*/g, '')).join('<p>');
+  document.querySelector('h1').innerText = 'Joke #' + i;
 }
-
 
 document.querySelector('#prev').onclick = () => setJoke(index - 1);
 document.querySelector('#random').onclick = () => setJoke();
